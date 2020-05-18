20c0158af798e5ea605d7faa2f2959032f9b993d
Fabian <fabivian@hotmail.com>
2020-05-18 18:47:10 -0500

    cambio index env6

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,6 +7,7 @@ use Zend\Diactoros\Response\HtmlResponse;
 class BaseController {
   protected $templateEngine;
     public function __construct(){
+      $templateEngine=null;
       $loader = new \Twig\Loader\FilesystemLoader('../views');
       $this->$templateEngine =  new \Twig\Environment($loader, [
         'debug' => true,
